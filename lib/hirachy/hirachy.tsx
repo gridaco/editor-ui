452cdbc34582f91781a1399ef6c4c9f937b799e7
GwonHeeJun <developer_gwon@naver.com>
2021-04-03 21:54:33 +0900

    add hirachy document ( tsx -> mdx )

--- lib/hirachy/hirachy.tsx
+++ lib/hirachy/hirachy.tsx
@@ -1,7 +1,34 @@
 import React from "react";
 import styled from "@emotion/styled";
 
-import HirachyItem from "./Item"
+import HirachyItem from "./Item";
+
+export const dummyData = [
+  {
+    id: "1",
+    title: "Item1",
+    type: "layout",
+    child: [
+      {
+        id: "id",
+        title: "Item",
+        type: "layout",
+      },
+    ],
+  },
+  {
+    id: "id",
+    title: "Item2",
+    type: "layout",
+    child: [
+      {
+        id: "id",
+        title: "Item",
+        type: "layout",
+      },
+    ],
+  },
+];
 
 type SceneType = "layout" | "text" | "icon" | "image";
 export interface Struct {
@@ -17,28 +44,29 @@ export interface HirachyProps {
   /** Expand HitachyItem id List */
   expandIds?: Array<string>;
   /** Scene Structs */
-  struct: Struct[];
+  structs: Struct[];
   /** Expand HitachyItem Function */
   onExpand: (id: string) => void;
 }
 
 function Hirachy(props: HirachyProps) {
-  const { level, struct, expandIds } = props;
+  const { level, structs, expandIds } = props;
 
   return (
     <Wrapper>
-      {struct.map((i, ix) => (
+      {structs.map((i, ix) => (
         <React.Fragment key={ix}>
           <HirachyItem
             struct={i}
             level={level}
+            isSelect={expandIds.includes(i.id)}
             onExpand={() => props.onExpand(i.id)}
           />
           {i.child && expandIds.includes(i.id) && (
             <Hirachy
               expandIds={expandIds}
               level={level + 1}
-              struct={i.child}
+              structs={i.child}
               onExpand={props.onExpand}
             />
           )}
@@ -49,13 +77,13 @@ function Hirachy(props: HirachyProps) {
 }
 
 Hirachy.defaultProps = {
-  level : 0,
+  level: 0,
   struct: [],
-  expandIds: []
-}
+  expandIds: [],
+};
 
 export default Hirachy;
 
 const Wrapper = styled.div`
-  background-color: #2A2E39;
+  background-color: #2a2e39;
 `;
