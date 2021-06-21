7d37825ce849cf1acefbe6818f75a2210a79a4f8
softmarshmallow <woojoo@softmarshmallow.com>
2021-06-22 07:52:05 +0900

    add multiplayer avatar group container component

--- packages/editor-ui-multiplayer/index.ts
+++ packages/editor-ui-multiplayer/index.ts
@@ -1,1 +1,2 @@
 export * from "./multiplayer-avatar";
+export * from "./multiplayer-avatar-group";
---
+++ packages/editor-ui-multiplayer/multiplayer-avatar-group/multiplayer-avatar-group.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+interface MultiplayerAvatarGroupProps {
+  spacing?: number;
+  children: JSX.Element[];
+}
+
+export function MultiplayerAvatarGroup(props: MultiplayerAvatarGroupProps) {
+  if (props.spacing !== undefined) {
+    const _spacingtype = spacingtype(props.spacing);
+    // todo
+  }
+
+  return (
+    <_Root>
+      {props.children.map((c) => {
+        return <Positioned spacing={props.spacing}>{c}</Positioned>;
+      })}
+    </_Root>
+  );
+}
+
+const _Root = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
+
+const Positioned = styled.div<{
+  spacing: number;
+  index?: number;
+}>`
+  position: relative;
+  margin-left: ${(p) => p.spacing + "px"};
+`;
+
+function spacingtype(spacing: number): "-" | "none" | "+" {
+  if (spacing > 0) {
+    return "+";
+  } else if (spacing < 0) {
+    return "-";
+  } else {
+    return "none";
+  }
+}
