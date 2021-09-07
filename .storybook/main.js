3ad3a48b8dfcc34dd5aeb1cab8ca5bd59f81dec7
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-07 17:49:58 +0900

    update storybook stories resolution pattern

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,15 +1,15 @@
 module.exports = {
-    "stories": [
-        "../lib/**/*.stories.mdx",
-        "../lib/**/*.stories.@(js|jsx|ts|tsx)"
-    ],
-    "addons": [
-        '@storybook/preset-create-react-app',
-        '@storybook/addon-actions',
-        '@storybook/addon-links',
-        '@storybook/addon-essentials',
-        '@storybook/preset-create-react-app',
-        '@storybook/addon-a11y',
-        '@storybook/addon-docs',
-    ],
-}
+  stories: [
+    "../packages/**/*.stories.mdx",
+    "../packages/**/*.stories.@(js|jsx|ts|tsx)",
+  ],
+  addons: [
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/preset-create-react-app",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
+  ],
+};
