811e8a1a6686f3449b6f3f9b987ef35480ba5de8
softmarshmallow <woojoo@softmarshmallow.com>
2021-09-07 19:23:53 +0900

    update exports

--- packages/editor-ui/lib/index.ts
+++ packages/editor-ui/lib/index.ts
@@ -1,2 +1,5 @@
 export * from "@editor-ui/hierarchy";
 export * as hooks from "@editor-ui/hooks";
+export * from "@editor-ui/button";
+export * from "@editor-ui/spacer";
+export * from "@editor-ui/utils";
