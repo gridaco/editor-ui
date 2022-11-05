aca8b73a57b4ddcfa4e39e0cb13e2d180e5eeca5
softmarshmallow <woojoo@softmarshmallow.com>
2022-11-06 01:13:37 +0900

    cleanup `@editor-ui/property` package

--- packages/editor-ui-property/input/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./background";
-export * from "./selector";
-export * from "./silder";
-export * from "./text-field";
-export * from "./toggle";
-// export * from "./check-box";
-// export * from "./dropdown";
