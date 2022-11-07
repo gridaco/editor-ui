0b71584dd561329bcbbd907700865ac8aab01d08
softmarshmallow <woojoo@softmarshmallow.com>
2022-11-07 14:47:02 +0900

    add sb preview with custom theme

--- packages/editor-ui-property/.storybook/preview.js
+++ packages/editor-ui-property/.storybook/preview.js
@@ -1,3 +1,7 @@
+import { useMemo } from "react";
+import { EditorPropertyThemeProvider } from "../theme";
+import * as themes from "../theme-one";
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,34 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
+
+export const withTheme = (Story, context) => {
+  const { theme: themeKey } = context.globals;
+  const theme = useMemo(() => themes[themeKey] || themes["light"], [themeKey]);
+
+  return (
+    <EditorPropertyThemeProvider theme={theme}>
+      <Story />
+    </EditorPropertyThemeProvider>
+  );
+};
+
+export const decorators = [withTheme];
+
+export const globalTypes = {
+  theme: {
+    name: "Theme",
+    title: "Theme",
+    description: "Theme for your components",
+    defaultValue: "light",
+    toolbar: {
+      icon: "paintbrush",
+      dynamicTitle: true,
+      items: [
+        { value: "light", left: "☀️", title: "Light mode" },
+        { value: "dark", left: "🌙", title: "Dark mode" },
+      ],
+    },
+  },
+};
