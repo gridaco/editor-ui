2b9ce8378966b0ce7bd0b99ceb24f30e5be60506
Universe <woojoo@softmarshmallow.com>
2022-11-20 06:59:03 +0900

    add optional padding props to property-lines

--- packages/editor-ui-property/property-lines/property-lines.tsx
+++ packages/editor-ui-property/property-lines/property-lines.tsx
@@ -1,13 +1,23 @@
-import styled from "@emotion/styled";
 import React from "react";
+import styled from "@emotion/styled";
 
-export function PropertyLines({ children }: React.PropsWithChildren<{}>) {
-  return <PropertyLinesContainer>{children}</PropertyLinesContainer>;
+export function PropertyLines({
+  children,
+  padding = 16,
+}: React.PropsWithChildren<{ padding?: React.CSSProperties["padding"] }>) {
+  return (
+    <PropertyLinesContainer
+      style={{
+        padding,
+      }}
+    >
+      {children}
+    </PropertyLinesContainer>
+  );
 }
 
 const PropertyLinesContainer = styled.div`
   display: flex;
   flex-direction: column;
   gap: 8px;
-  padding: 16px;
 `;
